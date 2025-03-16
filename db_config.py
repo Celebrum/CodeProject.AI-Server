023380598b7acfc132efc62715d12ce3f625042e
jean-sebastien beaulieu <jeansebastienbeaulieuscrde.01gmail.com>
2025-03-15 23:11:48 -0400

    Add database configuration for PostgreSQL, MongoDB, and MindsDB

---
+++ db_config.py
@@ -0,0 +1,19 @@
+POSTGRESQL_CONFIG = {
+    'host': 'localhost',
+    'database': 'your_database',
+    'user': 'your_user',
+    'password': 'your_password',
+    'port': '5432'
+}
+
+MONGODB_CONFIG = {
+    'host': '127.0.0.1',
+    'port': 47336,
+    'database': 'mindsdb'
+}
+
+MINDSDB_CONFIG = {
+    'host': '127.0.0.1',
+    'port': 47336,
+    'database': 'mindsdb'
+}
